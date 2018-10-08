a826f101de06e6ff21a2fc3f7f07abf3fee630de
k33theod <k33theod@gmail.com>
2018-10-08 18:17:51 +0200

    Update pattern_recognition_python.py

--- pattern_recognition_python.py
+++ pattern_recognition_python.py
@@ -5,9 +5,7 @@ def find_patern(a):
 		if a[:lena]==a[:i]*int(lena/i):
 			print(a[:i]+ ' is a patern of '+a)
 			print ("there is garbage in your sequence :"+a[lena+1:])
-			return
+			return a[:i]
 		i+=1
 	print ('No patern')
 
-for key in l_keys:
-	A[key[0]][key[1]]=scor_count[key]
